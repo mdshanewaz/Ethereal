126682e34ec56d33b33c204be5198275908e9a00
Md Sha Newaz <sawonnstu@gmail.com>
2024-12-27 14:04:44 +0600

    27

--- About_App/urls.py
+++ About_App/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from About_App import views
 
-app_name = 'Product_App'
+app_name = 'About_App'
 
 urlpatterns = [
-    path('', views.about_view, name='home'),
+    path('', views.home_view, name='home'),
+    path('about/', views.about_view, name='about'),
+    path('feature/', views.feature_view, name='feature'),
 ]
--- About_App/views.py
+++ About_App/views.py
@@ -2,11 +2,30 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+def home_view(request):
+    title = 'EHEREAL An English Medium School'
+
+    context = {
+        'title' : title,
+    }
+
+    return render(request, 'About_App/home.html', context=context)
+
+
 def about_view(request):
-    title = 'EHEREAL an English Medium School'
+    title = 'About | Ethereal'
 
     context = {
         'title' : title,
     }
 
     return render(request, 'About_App/about.html', context=context)
+
+def feature_view(request):
+    title = 'Feature | Ethereal'
+
+    context = {
+        'title' : title,
+    }
+
+    return render(request, 'About_App/feature.html', context=context)
