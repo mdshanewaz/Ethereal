12bbc8b5c1290606b152a024e40c08de7cfec6c8
Md Sha Newaz <sawonnstu@gmail.com>
2025-01-26 22:22:18 +0600

    26th Jan

--- Academic_App/urls.py
+++ Academic_App/urls.py
@@ -6,4 +6,7 @@ app_name = 'Academic_App'
 urlpatterns = [
     path('curriculum/', views.curriculum_view, name='curriculum'),
     path('syllabus/', views.syllabus_view, name='syllabus'),
+    path('booklist/', views.bookList_view, name='book'),
+    path('routine/', views.classSchedule_view, name='routine'),
+    path('exam/', views.examSchedule_view, name='exam'),
 ]
--- Academic_App/views.py
+++ Academic_App/views.py
@@ -19,3 +19,30 @@ def syllabus_view(request):
     }
 
     return render(request, 'Academic_App/syllabus.html', context=context)
+
+def bookList_view(request):
+    title = 'Book List | EHEREAL'
+
+    context = {
+        'title' : title,
+    }
+
+    return render(request, 'Academic_App/book_list.html', context=context)
+
+def classSchedule_view(request):
+    title = 'Class Schedule | EHEREAL'
+
+    context = {
+        'title' : title,
+    }
+
+    return render(request, 'Academic_App/class_schedule.html', context=context)
+
+def examSchedule_view(request):
+    title = 'Exam Schedule | EHEREAL'
+
+    context = {
+        'title' : title,
+    }
+
+    return render(request, 'Academic_App/exam_schedule.html', context=context)
