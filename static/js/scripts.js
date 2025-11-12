1d37c96a78c5259ebd9afbbfe2d63043fb4b8e08
MD. SHA NEWAZ <70477038+mdshanewaz@users.noreply.github.com>
2025-11-12 16:28:08 +0600

    Add files via upload

---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    document.getElementById('poster_contain').style.display = 'block';
+    this.document.getElementById('close_poster').onclick = function () {
+        document.getElementById('poster_contain').style.display = 'none';
+    }
+}
